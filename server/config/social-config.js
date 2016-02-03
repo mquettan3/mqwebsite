4507fd7d0681d9c93159ad9ca5d71e837d35cd11
Marcus Quettan <marcus.quettan@gtri.gatech.edu>
2016-02-03 23:57:20 +0000

    More pruning.

--- server/config/social-config.js
+++
@@ -1,9 +0,0 @@
-
-ServiceConfiguration.configurations.upsert(
-  {service: 'facebook'},
-  { $set: {appId: Meteor.settings.private.facebook_app_id, secret: Meteor.settings.private.facebook_secret} }
-);
-ServiceConfiguration.configurations.upsert(
-  {service: 'google'},
-  { $set: {clientId: Meteor.settings.private.google_client_id, secret: Meteor.settings.private.google_secret} }
-);
