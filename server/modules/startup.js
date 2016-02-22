176129f549c5a52b9c00f786154268e26e35b920
Marcus Quettan <marcus.quettan@gtri.gatech.edu>
2016-02-22 05:42:59 +0000

    Removing comments from the server startup.js

--- server/modules/startup.js
+++ server/modules/startup.js
@@ -1,21 +1,6 @@
 let startup = () => {
-  //_setBrowserPolicies();
-  //_generateAccounts();
-  //_generatePeople();
   _setEnvironmentVariables();
 };
-
-/*let _setBrowserPolicies = () => {
-  BrowserPolicy.content.allowOriginForAll( '*.amazonaws.com' );
-  BrowserPolicy.content.allowOriginForAll( '*.googleapis.com' );
-  BrowserPolicy.content.allowOriginForAll( '*.gstatic.com' );
-  BrowserPolicy.content.allowOriginForAll( '*.rhgaming.com' );
-  BrowserPolicy.content.allowOriginForAll('*.bootstrapcdn.com');
-};*/
-
-//let _generateAccounts = () => Modules.server.generateAccounts();
-
-//let _generatePeople = () => Modules.server.generatePeople();
 
 let _setEnvironmentVariables = () => Modules.server.setEnvironmentVariables();
 
