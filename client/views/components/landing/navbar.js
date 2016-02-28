e9fd4203a7efab3ab53a750b7f77ad4656aac7a1
Marcus Quettan <m_quettan@gatech.edu>
2016-02-28 18:41:41 +0000

    Completed adding the Mazel navbar to my site.

--- client/views/components/landing/navbar.js
+++ client/views/components/landing/navbar.js
@@ -1,7 +1,52 @@
+
+
+Template.navbar.onCreated(function () {
+  var handledScroll = false;
+  var addFixedHeaderOn = 1;
+  var addPrepareHeaderOn = 200;
+  var docElem = document.documentElement;
+
+  window.addEventListener('scroll', function (event) {
+      if (!handledScroll) {
+          handledScroll = true;
+          handleCurrentScroll();
+      }
+  }, false);
+
+  function handleCurrentScroll() {
+    var sy = scrollY();
+    if ( sy > addFixedHeaderOn ) {
+      $(".header").addClass('header-fixed');
+    }
+    else {
+      $(".header").removeClass('header-fixed');
+    }
+
+    if ( sy > addPrepareHeaderOn ) {
+      $(".header").addClass('header-prepare');
+    }
+    else {
+      $(".header").removeClass('header-prepare');
+    }
+
+    //Make sure this gets called again.
+    handledScroll = false;
+  }
+
+  function scrollY() {
+      return window.pageYOffset || docElem.scrollTop;
+  }
+
+});
+
+Template.navbar.events({
+
+});
+
 Template.navbar.onRendered(function () {
 
 });
 
-Template.landing.destroyed = function () {
+Template.navbar.destroyed = function () {
 
 };
--- client/views/layouts/landing.js
+++ client/views/layouts/landing.js
@@ -2,7 +2,21 @@ Template.landingLayout.rendered = function(){
 
     // Add gray color for background in blank layout
     $('body').addClass('gray-bg');
-    Modules.client.landing_dynamics_onRendered();
+    $('body').addClass('landing-page');
+
+    $('body').scrollspy({
+        target: '.header-fixed',
+        offset: 65
+    })
+
+    // Page scrolling feature
+    $('a.page-scroll').bind('click', function (event) {
+        var link = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(link.attr('href')).offset().top - 50
+        }, 500);
+        event.preventDefault();
+    });
 
 }
 
@@ -10,6 +24,6 @@ Template.landingLayout.destroyed = function(){
 
     // Remove special color for blank layout
     $('body').removeClass('gray-bg');
-    Modules.client.landing_dynamics_destroyed();
+    $('body').removeClass('landing-page');
 
 };
