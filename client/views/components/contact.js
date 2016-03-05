5b2f94e4a6ec416f294032422a8984bc2dff1a88
Marcus Quettan <m_quettan@gatech.edu>
2016-03-05 13:45:47 +0000

    Referencing form values for emails.

--- client/views/components/contact.js
+++ client/views/components/contact.js
@@ -9,12 +9,14 @@ Template.contact.events({
   // Form Submission 
   // ----------------------------------------------------------------
   'submit #contact-footer-form': function (event) {
+      var default_subject = 'MQWebsite Email From: ';
+      var subject = default_subject.concat(event.target.name.value);
       //In your client code: asynchronously send an email
       Meteor.call('sendEmail',
         'mquettan@gmail.com', //to
-        'mquettan@gmail.com', //from
-        'Hello from Meteor!', //subject
-        'This is a test of Email.send.'); //content
+        event.target.email.value, //from
+        subject,//subject
+        event.target.message.value); //content
       
       return false;
   }
