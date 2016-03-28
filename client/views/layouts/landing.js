75e84e53924a26791a97cfc59f98661fa5f77127
Marcus Quettan <m_quettan@gatech.edu>
2016-03-28 22:52:53 +0000

    Updated less files to work with firefox.  Also updated firefox.  Apparently the old version of firefox does not like the transition property.

--- client/views/layouts/landing.js
+++ client/views/layouts/landing.js
@@ -11,9 +11,9 @@ Template.landingLayout.rendered = function(){
     window.onload = function() {
         if(!loaded) {
             //Force the pre-loader to exist for at least 1 second.  Anything less is pretty jarring to see.
-            // setTimeout(function() { // MAde the pre-loader not have a default wait time because Ryuga complained.
+            setTimeout(function() { // Made the pre-loader not have a default wait time because Ryuga complained.
                 $('.loader-wrapper').addClass('animated fadeOut');
-            // }, 1000);
+            }, 1000);
             $('.loader-wrapper').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
                 $('.loader-wrapper').remove();
             })
